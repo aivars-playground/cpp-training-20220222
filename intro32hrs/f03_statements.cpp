9542f8ee7ae75c31e5ca0f4b648167b5fc311ea4
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2022-03-19 19:00:00 +0200

    refactoring

---
+++ intro32hrs/f03_statements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//each line ending with a ";" is a statement
+int main()
+{
+    int first = 12;
+    int second = 9;
+
+    int sum = first + second;
+
+    std::cout << "the sum of two numbers is:" << sum << std::endl;
+
+    return 0;
+}
