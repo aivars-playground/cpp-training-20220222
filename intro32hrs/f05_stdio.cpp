e4adcb36e3ab5a792683c3c4b9fc4e0f0d5f8a65
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2022-03-20 10:13:35 +0200

    iostream examples

--- intro32hrs/f05_stdio.cpp
+++ intro32hrs/f05_stdio.cpp
@@ -2,12 +2,28 @@
 
 int main()
 {
-    std::cout << "OUT" << std::endl;
-    std::clog << "LOG" << std::endl;
-    std::cerr << "ERR" << std::endl;
+    std::cout << "this is OUT level" << std::endl;
+    std::clog << "this is LOG level" << std::endl;
+    std::cerr << "this is ERR level" << std::endl;
 
+    //enter something like 2 3 4 (with spaces) and it will populate age, nr1 and nr2 below....
     int age;
-    std::cout << "input age:";
+    std::cout << "input age: ";
     std::cin >> age;
     std::cout << "received age:" << age << std::endl;
+
+    int day;
+    std::string month;
+    int year;
+    std::cout << "input birth date dd Month YYYY (separated by spaces): ";
+    std::cin >> day >> month >> year;
+    std::cout << "received: day=" << day << " , month=" << month <<  " , year=" << year << std::endl;
+
+    //note comment above
+    //see https://stackoverflow.com/questions/5739937/using-getlinecin-s-after-cin
+    std::cin.ignore(256, '\n');
+    std::string full_name;
+    std::cout << "read data with spaces: input full name (1 to n): ";
+    std::getline(std::cin, full_name);
+    std::cout << "received name: " << full_name << std::endl;    
 }
