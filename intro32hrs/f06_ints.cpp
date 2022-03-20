af56bb9917289da19a7d264ff6a42d80d8d9c629
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2022-03-20 12:47:28 +0200

    ints

--- intro32hrs/f06_ints.cpp
+++ intro32hrs/f06_ints.cpp
@@ -14,4 +14,35 @@ int main()
     std::cout << fifteen_octal << std::endl;
     std::cout << fifteen_hex << std::endl;
     std::cout << fifteen_binary << std::endl;
+
+    //modifiers
+
+    signed int max_signed = 2147483647;
+    std::cout << "max_signed:" << max_signed << " max_signed+1:" << (max_signed+1) << std::endl;
+
+    unsigned int max_unsigned = 4294967295;
+    std::cout << "max_unsigned:" << max_unsigned << " max_unsigned+1:" << (max_unsigned+1) << std::endl;
+
+    unsigned int minus_value = -5; //4294967291
+    std::cout << "minus_value:" << minus_value << std::endl;
+
+
+    //sizes
+
+    int int_val = 15;
+    std::cout << "int_val size:" << sizeof(int_val) << std::endl;
+
+    long long_val = 15; //4 bytes on 32 bit system, 8 bytes on 64 bit system
+    std::cout << "long_val size:" << sizeof(long_val) << std::endl;
+
+    long long longlong_val = 15;
+    std::cout << "longlong_val size:" << sizeof(longlong_val) << std::endl;
+
+    std::cout << "short int size:" << sizeof(short int) << std::endl;
+    std::cout << "unsigned short int size:" << sizeof(unsigned short int) << std::endl;
+    std::cout << "long size:" << sizeof(long) << std::endl;    // 4 or 8
+    std::cout << "long long size:" << sizeof(long long) << std::endl;
+
+    std::cout << "string size:" << sizeof(std::string) << std::endl;
+
 }
--- intro32hrs/f07_initialization.cpp
+++ intro32hrs/f07_initialization.cpp
@@ -11,6 +11,23 @@ int main()
     std::cout << "lions_count initialized to 0:" << lions_count << std::endl;
     std::cout << "dogs_count:" << dogs_count << std::endl;
 
-    int narrowing_conversion {2.9}; //throws compile time warning (or error???? depends on compiler)
-    std::cout << "narrowing_conversion:" << narrowing_conversion << std::endl;
+    int narrowing_conversion_assignment = 2.9;
+    std::cout << "narrowing_conversion_assignment:" << narrowing_conversion_assignment << std::endl;
+
+    // functional initializer would narrow down to 2
+    int narrowing_conversion_functional(2.9);
+    std::cout << "narrowing_conversion_functional:" << narrowing_conversion_functional << std::endl;
+
+    // //braced initalization would fail in my setup
+    // int narrowing_conversion_braced {2.9}; //throws compile time warning (or error???? depends on compiler)
+    // std::cout << "narrowing_conversion_braced:" << narrowing_conversion_braced << std::endl;
+
+    unsigned int unsiged_negative_assignemnt = -1;      //4294967295
+    unsigned int unsiged_negative_functional(-1);       //4294967295
+    //unsigned int unsiged_negative_braced{-1};         //compilation error
+
+    signed int minus_one = -1;
+    unsigned int reassignment_narrowing{minus_one};     //warning, got 4294967295
+    std::cout << "reassignment_narrowing:" << reassignment_narrowing << std::endl;
+
 }
